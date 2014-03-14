71f93b1de5346fc4d4502cc375c2c3f25b473cbc
Tewodros A <tedyhabtegebrial@gmail.com>
2014-03-14 09:23:12 +0300

    Experimenting with EMDeSTIN; Unnecessary comments have been removed

--- EMdestin_test/src/test2.cpp
+++ EMdestin_test/src/test2.cpp
@@ -26,100 +26,85 @@ using namespace cv;
 vector<Mat> spl;
 ImageSouceImpl isi;
 
-int main(int argc, char ** argv)
-{
-    printf("Here Every thing begins Fresh \n");
+int main(int argc, char ** argv) {
 
 
     /*-----------------------------------------initialization-------------------------------------------------*/
-            uint centroid_counts[]  = {16,8,8,8};
-            uint numLayers=4;
-            bool isUniform = true;
-            int size = 32*32;
-            SupportedImageWidths siw= W32;
-            //int extRatio = 1;
-            DestinNetworkAlt dn(siw, numLayers, centroid_counts, isUniform, 1);
-            string CifarDir="/home/opencog/Downloads/cifar-10-batches-bin";
-            // string CifarDir="/Downloads/cifar-10-batches-bin";
-            int TrainBatch=1;
-            int TestBatch=2;
-            int frameCount = 0;
-            //clock_t start,finish;
-            CifarSource csTrain(CifarDir,TrainBatch);
-            CifarSource csTest(CifarDir,TestBatch);
-            dn.setParentBeliefDamping(0);
-            dn.setPreviousBeliefDamping(0);
-            int maxCount =10000;
-            int bottom_belief_layer=0;
-     //printf("After Initialization \n");
+    uint centroid_counts[] = {16, 8, 8, 8};
+    uint numLayers = 4;
+    bool isUniform = true;
+    int size = 32 * 32;
+    SupportedImageWidths siw = W32;
+    DestinNetworkAlt dn(siw, numLayers, centroid_counts, isUniform, 1);
+    string CifarDir = "/home/opencog/Downloads/cifar-10-batches-bin";
+    int TrainBatch = 1;
+    int TestBatch = 2;
+    int frameCount = 0;
+    CifarSource csTrain(CifarDir, TrainBatch);
+    CifarSource csTest(CifarDir, TestBatch);
+    dn.setParentBeliefDamping(0);
+    dn.setPreviousBeliefDamping(0);
+    int maxCount = 10000;
+    int bottom_belief_layer = 0;
     /*----------------------------------------------training---------------------------------------*/
-        BeliefExporter be(dn,bottom_belief_layer);
-        int iterations_per_image=8;
-        //int layers=4;
-        bool isEnabled=true;
-        csTrain.disableAllClasses();
-        csTrain.setClassIsEnabled(0,isEnabled);
-        csTrain.setClassIsEnabled(1,isEnabled);
-        csTrain.setClassIsEnabled(2,isEnabled);
-        csTrain.setClassIsEnabled(3,isEnabled);
-        csTrain.setClassIsEnabled(4,isEnabled);
-        csTrain.setClassIsEnabled(5,isEnabled);
-        csTrain.setClassIsEnabled(6,isEnabled);
-        csTrain.setClassIsEnabled(7,isEnabled);
-        csTrain.setClassIsEnabled(8,isEnabled);
-        csTrain.setClassIsEnabled(9,isEnabled);
-        //printf("After Initialization Two\n");
-       // csTrain.setClassIsEnabled();
-       // csTest.setClassIsEnabled();
-        //for(int i=0;i<maxCount;++i){
-        int i=0;
-        for(int i=0;i<maxCount;++i){
-        //while(i<3){
-            //if (i%2==0)
-              //printf("Iteration Number: %d \n",i);
-           
-            csTrain.findNextImage();
-            dn.clearBeliefs();
-            for(int j   =0;j<numLayers;++j){
-                dn.setLayerIsTraining(j,false);//now
-                }
-            for(int j=0;j<numLayers;++j){
-                dn.setLayerIsTraining(j,true);
-                dn.doDestin(csTrain.getGrayImageFloat());
-                }
-            //string FileName="TestBeliefs.txt";
-            //be.CreateFile(FileName);
-            //be.DumpBeliefs(FileName);
-           for(int j=0;j<2;++j){
-               dn.setLayerIsTraining(j,true);
-               dn.doDestin(csTrain.getGrayImageFloat());
-           }
-            i++;
+    BeliefExporter be(dn, bottom_belief_layer);
+    int iterations_per_image = 8;
+    //int layers=4;
+    bool isEnabled = true;
+    csTrain.disableAllClasses();
+    csTrain.setClassIsEnabled(0, isEnabled);
+    csTrain.setClassIsEnabled(1, isEnabled);
+    csTrain.setClassIsEnabled(2, isEnabled);
+    csTrain.setClassIsEnabled(3, isEnabled);
+    csTrain.setClassIsEnabled(4, isEnabled);
+    csTrain.setClassIsEnabled(5, isEnabled);
+    csTrain.setClassIsEnabled(6, isEnabled);
+    csTrain.setClassIsEnabled(7, isEnabled);
+    csTrain.setClassIsEnabled(8, isEnabled);
+    csTrain.setClassIsEnabled(9, isEnabled);
 
+    for (int i = 0; i < maxCount; ++i) {
+        //while(i<3){
+        //if (i%200==0)
+        //printf("Iteration Number: %d \n",i);
+
+        csTrain.findNextImage();
+        dn.clearBeliefs();
+        for (int j = 0; j < numLayers; ++j) {
+            dn.setLayerIsTraining(j, false); //now
         }
+        for (int j = 0; j < numLayers; ++j) {
+            dn.setLayerIsTraining(j, true);
+            dn.doDestin(csTrain.getGrayImageFloat());
+        }
+        //string FileName="TestBeliefs.txt";
+        //be.CreateFile(FileName);
+        //be.DumpBeliefs(FileName);
+        for (int j = 0; j < 2; ++j) {
+            dn.setLayerIsTraining(j, true);
+            dn.doDestin(csTrain.getGrayImageFloat());
+        }
+        i++;
+
+    }
 
     /*-----------------------------------------------Testing/Dumping Beliefs to a File  ---------------------------------------------------*/
-        //  Destin * dn = network->getNetwork();
-        for(int j=0;j<numLayers;j++){
-            dn.setLayerIsTraining(j,false);
-            }
-        string FileName="TestBeliefs.txt";
-        be.CreateFile(FileName);
-        //printf("After Initialization Three\n");
-        for(int i=1;i<maxCount;++i){
-            // Show DestinImage(i)
-            csTest.setCurrentImage(i);
-            dn.clearBeliefs();
-            for(int j=0;j<numLayers;++j){
-                dn.doDestin(csTest.getGrayImageFloat());
-                }
-        //printf("After Initialization Four \n");
-            
-            be.DumpBeliefs(FileName);
+    for (int j = 0; j < numLayers; j++) {
+        dn.setLayerIsTraining(j, false);
+    }
+    string FileName = "TestBeliefs.txt";
+    be.CreateFile(FileName);
+    for (int i = 1; i < maxCount; ++i) {
+        csTest.setCurrentImage(i);
+        dn.clearBeliefs();
+        for (int j = 0; j < numLayers; ++j) {
+            dn.doDestin(csTest.getGrayImageFloat());
+        }
 
-            }
+        be.DumpBeliefs(FileName);
+
+    }
 
 
-    printf("Here We come to the end \n");
     return 0;
 }
